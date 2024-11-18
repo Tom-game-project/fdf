a4ad36d1c01739b97e57464af1038991bc5425a8
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-11-18 12:39:18 +0900

    add window and draw simple red line

--- src/fdf.c
+++ src/fdf.c
@@ -3,6 +3,14 @@
 #include <X11/keysym.h>
 #include "../minilibx-linux/mlx.h"
 
+
+// color #0x00RRGGBB
+
+// color definition
+#define RED    0x00FF0000
+#define GREEN  0x0000FF00
+#define BLUE   0x000000FF
+
 int event_handler(int key, void *mlx)
 {
 	(void) mlx;
@@ -12,12 +20,18 @@ int event_handler(int key, void *mlx)
 
 int main(void)
 {
-	void *mlx;
-	void *mlx_win;
-	
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 300, 300, "Hello World");
-	mlx_hook(mlx_win, KeyPress, KeyPressMask, event_handler, mlx);
-	mlx_loop(mlx);
+	void *mlx_ptr;
+	void *mlx_win; // canvas的な
+
+	mlx_ptr = mlx_init(); // 一番最初に必要
+
+	mlx_win = mlx_new_window(mlx_ptr, 300, 300, "Hello World");
+	mlx_clear_window(mlx_ptr, mlx_win); 
+
+	for (int i = 0; i< 10; i++){
+		mlx_pixel_put(mlx_ptr, mlx_win, 10, i, RED);
+	}
+	mlx_hook(mlx_win, KeyPress, KeyPressMask, event_handler, mlx_ptr);
+	mlx_loop(mlx_ptr);
 }
 
