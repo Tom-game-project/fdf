428e1607041ef2460a29cea2b143bdc2aca3f336
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-12-27 02:05:28 +0900

    fix function `draw_line` format

--- src/draw/draw_line.c
+++ src/draw/draw_line.c
@@ -54,10 +54,11 @@ static bool int16_lt(int16_t a, int16_t b)
 
 static int16_t int16_my_func(int16_t a, int16_t b)
 {
-	if (a < b)
-		return (b - a);
-	else
-		return (a - b);
+	//if (a < b)
+	//	return (b - a);
+	//else
+	//	return (a - b);
+	return (abs(a - b));
 }
 
 static int16_t shift8_func(int16_t a, int16_t b)
@@ -66,40 +67,38 @@ static int16_t shift8_func(int16_t a, int16_t b)
 	return (a << 8);
 }
 
-void draw_line(void *mlx_ptr, void *mlx_win, t_line line, t_colordiff color_pair)
+typedef struct s_color_info t_color_info;
+struct s_color_info
 {
-	t_i32x2 d;
-	t_i32x2 s;
-	t_i32x2 err_e2;
-	t_u16x4 color_step;
-	t_u8x4 color_map;
+	t_u16x4 s;
+	t_u8x4 m;
+};
 
-	d = encode_i32x2( abs(decode_int_x(line.start) - decode_int_x(line.end)),
-		abs(decode_int_y(line.start) - decode_int_y(line.end)));
-	s = encode_i32x2( 2 * (decode_int_x(line.start) < decode_int_x(line.end)) - 1,
-		2 * (decode_int_y(line.start) < decode_int_y(line.end)) - 1);
-	err_e2 = encode_i32x2(decode_int_x(d) - decode_int_y(d), 0);
-	color_step = t_u16x4_map(t_u16x4_div_scalar(
-			t_u16x4_map(color_pair.start, color_pair.end, int16_my_func),
-			(int16_t) t_i32x2_max(d)), 0, shift8_func);
-	color_map = create_u16x4_bool_map(color_pair.start, color_pair.end, int16_lt);
+///
+void draw_line(void *mlx_ptr, void *mlx_win, t_line l, t_colordiff cp)
+{
+	t_i32x2 d,s,e;
+	t_color_info ci;
+
+	d = encode_i32x2( abs(decode_int_x(l.start) - decode_int_x(l.end)),
+		abs(decode_int_y(l.start) - decode_int_y(l.end)));
+	s = encode_i32x2( 2 * (decode_int_x(l.start) < decode_int_x(l.end)) - 1,
+		2 * (decode_int_y(l.start) < decode_int_y(l.end)) - 1);
+	e = encode_i32x2(decode_int_x(d) - decode_int_y(d), 0);
+	ci.s = t_u16x4_map(t_u16x4_div_scalar(t_u16x4_map(cp.start, cp.end, int16_my_func),
+		(int16_t) t_i32x2_max(d)), 0, shift8_func);
+	ci.m = create_u16x4_bool_map(cp.start, cp.end, int16_lt);
 	while (true)
 	{
-		mlx_pixel_put(mlx_ptr, mlx_win, decode_int_x(line.start),
-			       	decode_int_y(line.start), conv_u16x4_to_u8x4(color_pair.start));
-		color_pair.start = t_u16x4_cal_color(color_pair.start, color_step, color_map);
-		if (t_i32x2_eq(line.start, line.end))
+		mlx_pixel_put(mlx_ptr, mlx_win, decode_int_x(l.start),
+			decode_int_y(l.start), conv_u16x4_to_u8x4(cp.start));
+		cp.start = t_u16x4_cal_color(cp.start, ci.s, ci.m);
+		if (t_i32x2_eq(l.start, l.end))
 			break;
-		err_e2 = encode_i32x2(decode_int_x(err_e2), 2 * decode_int_x(err_e2));
-		if (decode_int_y(err_e2) > -decode_int_y(d))
-		{
-			err_e2 = t_i32x2_sub(err_e2, encode_i32x2(decode_int_y(d),0));
-			line.start = t_i32x2_add(line.start, encode_i32x2(decode_int_x(s), 0));
-		}
-		if (decode_int_y(err_e2) < decode_int_x(d))
-		{
-			err_e2 = t_i32x2_add(err_e2, encode_i32x2(decode_int_x(d),0));
-			line.start = t_i32x2_add(line.start, encode_i32x2(0, decode_int_y(s)));
-		}
+		e = encode_i32x2(decode_int_x(e), 2 * decode_int_x(e));
+		e = t_i32x2_sub(e, encode_i32x2(decode_int_y(d) * (decode_int_y(e) > -decode_int_y(d)),0));
+		l.start = t_i32x2_add(l.start, encode_i32x2(decode_int_x(s) * (decode_int_y(e) > -decode_int_y(d)), 0));
+		e = t_i32x2_add(e, encode_i32x2(decode_int_x(d) * (decode_int_y(e) < decode_int_x(d)),0));
+		l.start = t_i32x2_add(l.start, encode_i32x2(0, decode_int_y(s) * (decode_int_y(e) < decode_int_x(d))));
 	}
 }
