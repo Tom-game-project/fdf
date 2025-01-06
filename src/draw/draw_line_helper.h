5fbf91c67a186e7f6c3a495aba17c57846a7433f
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2025-01-06 16:59:51 +0900

    [FIX] fix norminette

--- src/draw/draw_line_helper.h
+++ src/draw/draw_line_helper.h
@@ -6,7 +6,7 @@
 /*   By: tmuranak <tmuranak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 16:27:59 by tmuranak          #+#    #+#             */
-/*   Updated: 2025/01/02 17:46:24 by tmuranak         ###   ########.fr       */
+/*   Updated: 2025/01/06 16:51:46 by tmuranak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@
 # include <stdbool.h>
 # include "draw.h"
 
-int		abs(int a);
-bool	int16_gt(uint16_t a, uint16_t b);
+int			abs(int a);
+bool		int16_gt(uint16_t a, uint16_t b);
 uint16_t	int16mf(uint16_t a, uint16_t b);
 uint16_t	shift8_func(uint16_t a, uint16_t b);
 
-void	draw_line_helper(t_mlx_ptr_win mlx_ptr_win, t_line l, \
+void		draw_line_helper(t_mlx_ptr_win mlx_ptr_win, t_line l, \
 		t_colordiff cp);
-void	draw_back_line_helper(t_mlx_ptr_win mlx_ptr_win, t_line l);
+void		draw_back_line_helper(t_mlx_ptr_win mlx_ptr_win, t_line l);
 
 #endif
